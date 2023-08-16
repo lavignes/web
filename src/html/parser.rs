cae933de112e253a4c05dff6c0f44662a821b40b
Scott LaVigne <lavignes@amazon.com>
2023-08-15 22:47:46 -0700

    checkpoint

--- src/html/parser.rs
+++ src/html/parser.rs
@@ -1,8 +1,32 @@
 use super::tokenizer::Tokenizer;
 use crate::dom::Dom;
 
+#[derive(thiserror::Error, Debug, Clone)]
+#[error("{msg}")]
+pub struct ParserError {
+    msg: String,
+}
+
+#[derive(Copy, Clone)]
+enum InsertionMode {
+    Initial,
+    BeforeHtml,
+    BeforeHead,
+    InHead,
+    AfterHead,
+    InBody,
+    AfterBody,
+    AfterAfterBody,
+}
+
 #[pin_project::pin_project]
 pub struct Parser<R> {
     #[pin]
     tokenizer: Tokenizer<R, Dom>,
+}
+
+impl<R> Parser<R> {
+    pub async fn parse(&mut self) -> Result<(), ParserError> {
+        todo!()
+    }
 }
